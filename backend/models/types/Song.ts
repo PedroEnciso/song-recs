fb965c2e706390129e9b733323b5821427f5a43e
Pedro Enciso <penciso@ucsd.edu>
2024-04-15 15:13:27 -0700

    add Song type

---
+++ backend/models/types/Song.ts
@@ -0,0 +1,7 @@
+export interface Song {
+  id: string;
+  album_name: string;
+  artist: string;
+  name: string;
+  album_photo_url: string;
+}
--- backend/models/types/index.ts
+++ backend/models/types/index.ts
@@ -1,2 +1,3 @@
 export type { Recommendation } from "./Recommendation";
 export type { Recommender } from "./Recommender";
+export type { Song } from "./Song";
