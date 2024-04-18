2fd31b1deee9051e7d3a6d9febb9802cbe877262
Pedro Enciso <penciso@ucsd.edu>
2024-04-18 11:41:49 -0700

    add RecommendationWithSong type

--- backend/models/types/Recommendation.ts
+++ backend/models/types/Recommendation.ts
@@ -1,8 +1,16 @@
 import type { Recommender } from "./Recommender";
+import { Song } from "./Song";
 
 export interface Recommendation {
   id: string;
   spotify_song_id: string;
+  recommender: string;
+  comment: string;
+}
+
+export interface RecommendationWithSong {
+  id: string;
+  song: Song;
   recommender: string;
   comment: string;
 }
--- backend/models/types/index.ts
+++ backend/models/types/index.ts
@@ -1,3 +1,3 @@
-export type { Recommendation } from "./Recommendation";
+export type { Recommendation, RecommendationWithSong } from "./Recommendation";
 export type { Recommender } from "./Recommender";
 export type { Song } from "./Song";
