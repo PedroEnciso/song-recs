e1b1ab97536a98562be101db0b7c494de6ffb91b
Pedro Enciso <penciso@ucsd.edu>
2024-04-19 14:25:55 -0700

    improve error andling

--- backend/controllers/songs.ts
+++ backend/controllers/songs.ts
@@ -5,15 +5,18 @@ const songsRouter = Router();
 
 songsRouter.get("/", async (req: Request, res: Response) => {
   const query = req.query.song as string;
+  if (!query || query === "") {
+    res.status(400).json({ message: "No query was defined" });
+  }
   try {
     const spotifySession = await SpotifySession();
     const songs = await spotifySession.getSongsByQuery(query);
     res.json(songs);
   } catch (error) {
     if (error instanceof Error) {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ message: error });
     } else {
-      res.status(500).json({ error: "Could not find songs." });
+      res.status(500).json({ message: "Could not find songs." });
     }
   }
 });
