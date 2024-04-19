083c280f6beab1f041fb81c5e336a089bd17362b
Pedro Enciso <penciso@ucsd.edu>
2024-04-19 11:02:58 -0700

    implement test database url string

--- backend/utils/mongoose.ts
+++ backend/utils/mongoose.ts
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MONGO_DB_URI =
+  process.env.NODE_ENV === "test"
+    ? (process.env.MONGODB_TEST_URL as string)
+    : (process.env.MONGODB_URL as string);
 
 try {
-  mongoose.connect(process.env.MONGODB_URL as string);
+  mongoose.connect(MONGO_DB_URI);
 } catch (err) {
   console.log("Could not connect to mongoDB", err);
 }
