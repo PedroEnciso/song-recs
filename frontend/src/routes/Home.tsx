bfd3ec08b1ed9f2acc3a429089398aae4ed476cb
Pedro Enciso <penciso@ucsd.edu>
2024-04-22 13:30:16 -0700

    style and content tweaks

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -3,12 +3,10 @@ import "./Home.css";
 function Home() {
   return (
     <div>
-      <h1>Welcome to my song rec app!</h1>
+      <h1 className="text-xl">Welcome to song recs</h1>
       <p className="info-text">
-        You can add a song to my spotify playlist using the form below. Songs
-        are not added automatically, so do not be alarmed if you do not
-        immediately see it in my playlist. You will be able to see the status of
-        your song rec in your rec history.
+        This app allows you to add a song to my spotify account. You will be
+        able to see the status of your song rec in your rec history.
       </p>
     </div>
   );
