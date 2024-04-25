f53f6cdb5cd3a370cbb1e245b91178fb3fe94207
Pedro Enciso <penciso@ucsd.edu>
2024-04-25 14:01:44 -0700

    query function to get songs

--- frontend/src/queries/getSongs.ts
+++ frontend/src/queries/getSongs.ts
@@ -1,4 +1,6 @@
-export async function getSongs(query: string) {
+import type { Song } from "../../../backend/models/types";
+
+export async function getSongs(query: string): Promise<Song[]> {
   const response = await fetch(`http://localhost:3000/api/songs?song=${query}`);
   if (!response.ok) {
     throw new Error("Caught this error");
