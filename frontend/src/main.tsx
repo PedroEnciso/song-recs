5696fa51be5618e3043d7a8b3db9a70dbefe667e
Pedro Enciso <penciso@ucsd.edu>
2024-05-01 14:45:24 -0700

    moved router into /routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // router imports
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import { router } from "./routes";
 // tanstack query imports
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
