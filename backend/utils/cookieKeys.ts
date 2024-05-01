560177f55ffe161555857b8eb28f0c7aab3aebcf
Pedro Enciso <penciso@ucsd.edu>
2024-05-01 12:48:28 -0700

    add rateLimitKey

--- backend/utils/cookieKeys.ts
+++ backend/utils/cookieKeys.ts
@@ -1,3 +1,4 @@
 export const userIdKey = "user_id";
 export const accessTokenKey = "spotify_access_token";
 export const refreshTokenKey = "spotify_refresh_token";
+export const rateLimitKey = "rate_limit";
