ff3a35c24929ce598c03d5dfd854915e93274ae6
Pedro Enciso <penciso@ucsd.edu>
2024-07-17 16:50:29 -0700

    getPlaylist and updateRecommendationStatus

--- backend/src/utils/dbAPI.ts
+++ backend/src/utils/dbAPI.ts
@@ -1,8 +1,15 @@
 import { db } from "../db/client";
 import type { InsertPlaylist } from "../db/schema";
-import { playlistTable } from "../db/schema";
+import { playlistTable, recommendationTable } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 const dbAPI = {
+  async getPlaylist(id: number) {
+    return await db.query.playlistTable.findFirst({
+      where: (playlists, { eq }) => eq(playlists.id, id),
+    });
+  },
+
   async insertPlaylist(newPlaylist: InsertPlaylist) {
     return await db.insert(playlistTable).values(newPlaylist).returning();
   },
@@ -11,6 +18,17 @@ const dbAPI = {
     return await db.query.recommendationTable.findMany({
       where: (recommendations, { eq }) => eq(recommendations.playlistId, id),
     });
+  },
+
+  async updateRecommendationStatus(
+    newStatus: "added" | "pending" | "rejected",
+    recommendationId: number
+  ) {
+    return await db
+      .update(recommendationTable)
+      .set({ status: newStatus })
+      .where(eq(recommendationTable.id, recommendationId))
+      .returning();
   },
 };
 
