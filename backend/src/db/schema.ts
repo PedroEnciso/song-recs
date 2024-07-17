1c83b9e1d667820e9fa19633eb7d56cbe2b58a8b
Pedro Enciso <penciso@ucsd.edu>
2024-07-17 16:14:54 -0700

    remove creatorTable

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -2,15 +2,10 @@ import { integer, pgTable, pgEnum, serial, varchar } from "drizzle-orm/pg-core";
 
 export const statusEnum = pgEnum("status", ["pending", "added", "rejected"]);
 
-export const creatorTable = pgTable("creator_table", {
-  id: serial("id").primaryKey(),
-  spotify_id: varchar("spotify_id").notNull(),
-});
 export const playlistTable = pgTable("playlist_table", {
   id: serial("id").primaryKey(),
-  creatorId: integer("creator_id")
-    .notNull()
-    .references(() => creatorTable.id, { onDelete: "cascade" }),
+  creatorId: varchar("creator_id").notNull(),
+  name: varchar("name").notNull(),
 });
 export const recommendationTable = pgTable("recommendation_table", {
   id: serial("id").primaryKey(),
@@ -22,8 +17,6 @@ export const recommendationTable = pgTable("recommendation_table", {
     .references(() => playlistTable.id, { onDelete: "cascade" }),
 });
 
-export type InsertCreator = typeof creatorTable.$inferInsert;
-export type SelectCreator = typeof creatorTable.$inferSelect;
 export type InsertPlaylist = typeof playlistTable.$inferInsert;
 export type SelectPlaylist = typeof playlistTable.$inferSelect;
 export type InsertRecommendation = typeof recommendationTable.$inferInsert;
