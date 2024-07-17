ec2b2e081dd2e4e5102c5a1436a35bdc206a41bd
Pedro Enciso <penciso@ucsd.edu>
2024-07-17 13:07:49 -0700

    updated types

--- backend/models/types/Recommendation.ts
+++ backend/models/types/Recommendation.ts
@@ -1,21 +1,23 @@
 import type { Recommender } from "./Recommender";
 import { Song } from "./Song";
 
-export interface Recommendation {
+interface RecommendationBase {
   id: string;
+  recommenderId: string;
+  status: "pending" | "added" | "rejected";
+}
+
+// recommendation submitted by anon users
+export interface Recommendation extends RecommendationBase {
   spotify_song_id: string;
-  recommender: string;
-  user_id: string;
-  comment: string;
 }
 
-export interface RecommendationWithSong {
-  id: string;
+// recommendation returned to creators
+export interface RecommendationWithSong extends RecommendationBase {
   song: Song;
-  recommender: string;
-  comment: string;
 }
 
+// unedited
 export interface RecommendationWithoutId {
   spotify_song_id: string;
   recommender: string;
